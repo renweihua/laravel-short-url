bbd2d19aa25912e97b24e2eccee5d6910ac5f72e
小丑路人 <2278757482@qq.com>
2023-03-17 14:20:01 +0800

    定义域名表与模型

---
+++ app/Modules/ShortUrl/Database/Migrations/2023_03_17_134533_create_urls_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = 'urls';
+        if (Schema::hasTable($table)) return;
+        Schema::create($table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->default(0)->comment('会员Id');
+            $table->string('long_url', 200)->default('')->comment('长域名');
+            $table->string('short_url', 200)->default('')->comment('短域名');
+            $table->boolean('is_private')->unsigned()->default(0)->comment('是否私密');
+            $table->boolean('is_hidden')->unsigned()->default(0)->comment('是否隐藏');
+            $table->integer('created_time')->unsigned()->default(0)->comment('创建时间');
+            $table->integer('updated_time')->unsigned()->default(0)->comment('更新时间');
+            $table->boolean('is_delete')->unsigned()->default(0)->comment('是否删除');
+            $table->index('user_id');
+            $table->unique('short_url');
+            $table->index('is_delete');
+        });
+        $table = get_db_prefix() . $table;
+        // 设置表注释
+        DB::statement("ALTER TABLE `{$table}` comment '域名表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
