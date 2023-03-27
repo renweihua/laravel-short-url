9aab4333296d099f3c680d924deb54069932093a
小丑路人 <2278757482@qq.com>
2023-03-27 16:48:37 +0800

    系统设置表与不同设备的域名配置表

---
+++ app/Modules/ShortUrl/Database/Migrations/2023_03_27_163855_create_settings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = 'settings';
+        if (Schema::hasTable($table)) return;
+        Schema::create($table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->string('key', 200)->default('')->comment('键名');
+            $table->string('value', 5000)->default('')->comment('键值');
+            $table->integer('created_time')->unsigned()->default(0)->comment('创建时间');
+            $table->integer('updated_time')->unsigned()->default(0)->comment('更新时间');
+            $table->boolean('is_delete')->unsigned()->default(0)->comment('是否删除');
+            $table->unique('key');
+            $table->index('is_delete');
+        });
+        $table = get_db_prefix() . $table;
+        // 设置表注释
+        DB::statement("ALTER TABLE `{$table}` comment '系统配置表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
---
+++ app/Modules/ShortUrl/Database/Migrations/2023_03_27_163934_create_device_targets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeviceTargetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = 'device_targets';
+        if (Schema::hasTable($table)) return;
+        Schema::create($table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->bigInteger('url_id')->unsigned()->default(0)->comment('Url的Id');
+            $table->integer('device_id')->unsigned()->default(0)->comment('设备编号枚举Id');
+            $table->string('target_url', 200)->default('')->comment('对应设置的URL');
+            $table->integer('created_time')->unsigned()->default(0)->comment('创建时间');
+            $table->integer('updated_time')->unsigned()->default(0)->comment('更新时间');
+            $table->boolean('is_delete')->unsigned()->default(0)->comment('是否删除');
+            $table->index('url_id');
+            $table->index('device_id');
+            $table->index('is_delete');
+        });
+        $table = get_db_prefix() . $table;
+        // 设置表注释
+        DB::statement("ALTER TABLE `{$table}` comment '不同设备的域名配置表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device_targets');
+    }
+}
